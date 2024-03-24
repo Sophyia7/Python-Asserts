639dd4a1e104b59668852f7f704ba1fca473e7e7
Sophyia7 <iroegbusophia3@gmail.com>
2024-03-24 22:06:33 +0100

    Deleted folder

--- use_cases/data_integrity.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=common/home")
-
-
-# `assert "Your Store" in driver.title, "Website title is not as expected"`: This line is where the data integrity check happens. The `driver.title` property returns the title of the current page, and the `assert` statement checks if the string "Your Store" is in that title.
-assert "Your Store" in driver.title, "Website title is not as expected"
