c242d6650db19886425911e95bc5ddd2530caa57
Sophyia7 <iroegbusophia3@gmail.com>
2024-04-30 11:13:53 +0100

    Added lambda hook

--- base.py
+++ base.py
@@ -18,7 +18,7 @@ class SampleTest(unittest.TestCase):
             "user": username,
             "accessKey": access_key,
             "build": "Python Asserts Tests with Selenium",
-            "name": "Python Asserts Tests with Selenium",
+            "name": "Python Asserts Tests",
             "platformName": "Windows 11",
             "w3c": True,
             "browserName": "Chrome",
@@ -32,6 +32,9 @@ class SampleTest(unittest.TestCase):
         self.driver = webdriver.Remote(
             command_executor=f"https://{username}:{access_key}@hub.lambdatest.com/wd/hub",
             options=browser_options)
+        
+        # Indicate that the test has passed since the Assert is not raised
+        self.driver.execute_script("lambda-status=passed")
 
 
 if __name__ == "__main__":
